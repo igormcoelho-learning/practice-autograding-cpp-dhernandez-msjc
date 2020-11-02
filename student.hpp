7b221b27c487f154687f7bd1c6947764b8bc3cbc
David C Hernandez, MSW, MBA <dhernandez@msjc.edu>
2020-11-01 16:30:34 -0800

    testing no change.

--- student.hpp
+++ student.hpp
@@ -13,11 +13,11 @@ int mul(int a, int b);
 
 int add(int a, int b)
 {
-   return 0; // TODO: fix
+   return 0;
 }
 
 // Exercise 2: implements function 'mul', that multiplies two numbers
 int mul(int a, int b)
 {
-   return 0; // TODO: fix
+   return 0;
 }
